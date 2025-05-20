0844ded46167ffb2202907354c5b2f4e7006097b
Anas Mounsif <46957186+anasmounsif@users.noreply.github.com>
2025-05-20 15:11:04 +0200

    Rollback navbar and added Firebase

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,8 +2,8 @@ import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
-import {providePrimeNG } from 'primeng/config';
-import {provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { providePrimeNG } from 'primeng/config';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import Aura from '@primeng/themes/aura';
 
 export const appConfig: ApplicationConfig = {
@@ -14,6 +14,9 @@ export const appConfig: ApplicationConfig = {
     providePrimeNG({
       theme: {
         preset: Aura,
+        options: {
+          darkModeSelector: false || 'none',
+        },
       },
     }),
   ],
--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,9 +1,8 @@
 import { Component } from '@angular/core';
-import { NavComponent } from '../../components/nav/nav.component';
 
 @Component({
   selector: 'app-home',
-  imports: [NavComponent],
+  imports: [],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss',
 })
