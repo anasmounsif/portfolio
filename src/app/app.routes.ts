6920f34742fc9e3795054c5d74a99a3709cd07f1
Anas Mounsif <46957186+anasmounsif@users.noreply.github.com>
2025-06-20 11:39:52 +0200

    Removed navbar in order to simplify the view, added projects feature over home,

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,11 +1,10 @@
 import { Routes } from '@angular/router';
 
 export const routes: Routes = [
-  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: '', redirectTo: 'projects', pathMatch: 'full' },
   {
-    path: 'home',
-    loadComponent: () =>
-      import('./pages/home/home.component').then((c) => c.HomeComponent),
+    path: 'projects',
+    loadChildren: () =>
+      import('./features/projects/reports.routes').then((c) => c.routes),
   },
-  { path: '**', redirectTo: 'home', pathMatch: 'full' },
 ];
---
+++ src/app/features/projects/model/project-status.enum.ts
@@ -0,0 +1,5 @@
+export enum ProjectStatus {
+  completed,
+  developing,
+  ideating,
+}
